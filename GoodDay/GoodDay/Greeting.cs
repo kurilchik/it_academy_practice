e175ce4a185d3ced8ce55b2273518421dcf805d8
anastasiaShpakava <shpakavaa@gmail.com>
2020-11-11 20:01:08 +0300

    lecture_6

---
+++ GoodDay/GoodDay/Greeting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodDay
+{
+    class Greeting
+    {
+        public static void Time()
+        {
+            TimeSpan now = DateTime.Now.TimeOfDay;
+            if (now.Hours >= 9 || now.Hours < 12)
+            {
+                Console.WriteLine("Good morning, guys");
+            }
+        }
+    }
+}
