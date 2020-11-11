09c11bedd2cfd02af96506bbece984e0512028a6
anastasiaShpakava <shpakavaa@gmail.com>
2020-11-11 20:16:03 +0300

    lecture6.task1

--- GoodDay/GoodDay/Greeting.cs
+++ GoodDay/GoodDay/Greeting.cs
@@ -9,10 +9,20 @@ namespace GoodDay
         public static void Time()
         {
             TimeSpan now = DateTime.Now.TimeOfDay;
-            if (now.Hours >= 9 || now.Hours < 12)
+            if (now.Hours >= 9 && now.Hours < 12)
             {
                 Console.WriteLine("Good morning, guys");
             }
+            else if (now.Hours >= 12 && now.Hours < 15)
+            {
+                Console.WriteLine("Good day, guys");
+            }
+            else if (now.Hours >= 15 && now.Hours < 22)
+            {
+                Console.WriteLine("Good day, guys");
+            }
+            else
+                Console.WriteLine("Good night, guys");
         }
     }
 }
