eb58b257d9db8784cbf01fa7147a7ae4fa791c4b
Andrew Kurilchik <andrew.kurilchik@gmail.com>
2020-11-16 20:39:49 +0300

    Task2

--- Practice/lecture7/Program.cs
+++ Practice/lecture7/Program.cs
@@ -7,11 +7,28 @@ namespace lecture7
         static void Main(string[] args)
         {
             EmptyArray();
+            SumArray();
 
             static void EmptyArray()
             {
                 int length = 0;
                 object[] emptyArray = new object[length];
+            }
+
+            static void SumArray()
+            {
+                object[] sumArray = new object[3] { 32, 'A', "Hello" };
+                foreach (var item in sumArray)
+                {
+                    Console.WriteLine(item);
+                }
+
+                sumArray[0] = (int)sumArray[0] + 10;
+                sumArray[2] = (string)sumArray[2] + ", guys!";
+                foreach (var item in sumArray)
+                {
+                    Console.WriteLine(item);
+                }
             }
         }
     }
