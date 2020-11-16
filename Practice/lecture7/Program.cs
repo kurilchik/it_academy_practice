f1227d8547d083246ab8642b51f4605ffcc7b8ba
Andrew Kurilchik <andrew.kurilchik@gmail.com>
2020-11-16 20:31:07 +0300

    Task1

---
+++ Practice/lecture7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lecture7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmptyArray();
+
+            static void EmptyArray()
+            {
+                int length = 0;
+                object[] emptyArray = new object[length];
+            }
+        }
+    }
+}
