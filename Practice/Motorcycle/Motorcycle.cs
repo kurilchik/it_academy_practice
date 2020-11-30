00a9faf1e01eb716a814ae0feae0454abd9cc9b2
Andrew Kurilchik <andrew.kurilchik@gmail.com>
2020-11-30 19:59:47 +0300

    lecture10

---
+++ Practice/Motorcycle/Motorcycle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Motorcycle
+{
+    class Motorcycle
+    {
+        private int _vin;
+
+        public int Vin
+        {
+            get { return _vin; }
+            set { _vin = value; }
+        }
+
+        private int _model;
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        private string _manufacturer;
+
+        public int Manufacturer
+        {
+            get { return _manufacturer; }
+            set { _manufacturer = value; }
+        }
+
+        private int _yearOfIssue = 2020;
+
+        public int YearOfIssue
+        {
+            get { return _yearOfIssue; }
+            private set
+        }
+
+        private int _mileage;
+
+        public int Mileage
+        {
+            get { return _mileage; }
+            set { _mileage = value; }
+        }
+
+
+
+    }
+}
