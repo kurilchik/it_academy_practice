69fc2f27c988073ab153eadf93149503aa91c2fd
Andrew Kurilchik <andrew.kurilchik@gmail.com>
2020-12-07 19:45:24 +0300

    lecture 12

---
+++ Practice/OOP_Figures/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Figures
+{
+    class Triangle : IFigure
+    {
+        public int Value { get; set; }
+
+        public double Area { get; set; }
+
+        public void AreaCalc()
+        {
+            double h = Math.Sqrt(Math.Pow(Value, 2) - Math.Pow(Value, 2) / 4);
+            Area = (Value * h) / 2;
+        }
+    }
+}
