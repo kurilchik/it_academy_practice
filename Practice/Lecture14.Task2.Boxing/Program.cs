54ddd31cf04690c6501d7f725250bb5bcdef31f7
Andrew Kurilchik <andrew.kurilchik@gmail.com>
2020-12-14 19:22:38 +0300

    Lecture14.Task2

---
+++ Practice/Lecture14.Task2.Boxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace Lecture14.Task2.Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            //System.Int32
+            arrayList.Add(45); //true, boxing
+            //System.Boolean
+            arrayList.Add(true); //true, boxing
+            //System.String
+            arrayList.Add("Hello!"); //false
+            //System.Double
+            arrayList.Add(23.45); //true, boxing
+        }
+    }
+}
