5c092c10f93042b120ff401829826c1603c0818d
Andrew Kurilchik <andrew.kurilchik@gmail.com>
2020-12-14 19:39:59 +0300

    Lecture14.Task1

---
+++ Practice/Lecture14.Task1.StructRectangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lecture14.Task1.StructRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle[] rectangles = new Rectangle[100];
+
+            Random random = new Random();
+            for (int i = 0; i < rectangles.Length; i++)
+            {
+                rectangles[i].height = random.Next(1, 10);
+                rectangles[i].width = random.Next(1, 10);
+            }
+
+            for (int i = 0; i < rectangles.Length; i++)
+            {
+                Console.WriteLine($"height: {rectangles[i].height} x width: {rectangles[i].width}");
+            }
+        }
+    }
+}
