626537a6466b241d7b3497e9ffa5274c7b114383
Andrew Kurilchik <andrew.kurilchik@gmail.com>
2020-12-21 19:20:38 +0300

    Lecture15.Task2

---
+++ Practice/Lecture15.Task2/StringExtansions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture15.Task2
+{
+    public static class StringExtansions
+    {
+        public static string ShortString(this string str)
+        {
+            string result = str.Remove(5);
+            result = result + "...";
+            return result;
+        }
+    }
+}
